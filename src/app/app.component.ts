aaaa4fbb94d1fb3c592aa343c7d44118a2dc7272
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-11-07 21:05:14 +0300

    Initialize repo and add main folders,files,code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick(){
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrtuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    
+
+    let validCharacters = '';
+    if (this.includeLetters){
+      validCharacters += letters;
+    }
+    if (this.includeNumbers){
+      validCharacters += numbers;
+    }
+    if (this.includeSymbols){
+      validCharacters += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validCharacters.length);
+      generatedPassword += validCharacters[index];
+    }
+    this.password = generatedPassword;
+  }
+
 }
